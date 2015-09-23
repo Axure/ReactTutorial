4a47b23c805636bbc11924383cdf38095c42a99a
lulucici <yolandachen7@zju.edu.cn>
2015-09-23 18:14:43 +0800

    A initial version that runs.

---
+++ src/components/Id.jsx
@@ -0,0 +1,56 @@
+var Ids = [
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    },
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    },
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    }
+    ,
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    }
+    ,
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    }
+    ,
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    },
+    {
+        name: "Yulu Chen",
+        age: "20",
+        gender: "female",
+        email: "lulucici77@gmail.com",
+        intro: "This is a gaoleng de doubi."
+    }]
+
+// Realize edit and add, delete.
+
