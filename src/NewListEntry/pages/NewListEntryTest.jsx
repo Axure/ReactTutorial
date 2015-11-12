e4e27bc617ff37ba8b8e6d031405732b9676aa85
Axure <axurez@vip.qq.com>
2015-11-13 07:22:32 +0800

    Modify directory structure.

--- src/NewListEntry/pages/NewListEntryTest.jsx
+++ src/NewListEntry/pages/NewListEntryTest.jsx
@@ -1,4 +1,4 @@
-var NewListEntry = require('../../src/components/NewListEntry.jsx');
+var NewListEntry = require('../components/NewListEntry.jsx');
 var React = require('react');
 
 React.render(<NewListEntry />, document.body);
